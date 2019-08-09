c61245c7709d65df24834216c6371e657429c83a
Fernanda Cacho <flemes19@gmail.com>
2019-08-09 19:27:36 -0300

    Add files via upload

---
+++ Apoia/Apoia/MateriaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MateriaTableViewCell.swift
+//  Apoia
+//
+//  Created by student on 08/08/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class MateriaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
